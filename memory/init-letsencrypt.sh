8f5b6c3e18e43521e873b2d6d5d187e79c1305dc
sunprizrak <sunprizrak@gmail.com>
2023-12-09 02:38:18 +0300

    ...

--- memory/init-letsencrypt.sh
+++ memory/init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=(yodco.org www.yodco.org)
+domains=(adjacency.by www.adjacency.by)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="info@yodco.org" # Adding a valid address is strongly recommended
+email="site.adjacency@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
